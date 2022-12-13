eefa13685ef2cbc972128613d1cb8b1a1de125b6
chillA711 <emj7job@gmail.com>
2022-12-13 07:49:15 -0800

    alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,24 +1,24 @@
-#include <stdio.h>                                                                                                                     
-/**                                                                                                                                    
-*main - Entry point                                                                                                                    
-*                                                                                                                                      
-*Return: Always 0 (Success)                                                                                                            
-*/                                                                                                                                     
-int main(void)                                                                                                                         
-{                                                                                                                                      
-char c, d;                                                                                                                             
-c = 'a';                                                                                                                               
-d = 'A';                                                                                                                               
-while (c <= 'z')                                                                                                                       
-{                                                                                                                                      
-putchar(c);                                                                                                                            
-c++;                                                                                                                                   
-}                                                                                                                                      
-while (d <= 'Z')                                                                                                                       
-{                                                                                                                                      
-putchar(d);                                                                                                                            
-d++;                                                                                                                                   
-}                                                                                                                                      
-putchar('\n');                                                                                                                         
-return (0);                                                                                                                            
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+char c, d;
+c = 'a';
+d = 'A';
+while (c <= 'z')
+{
+putchar(c);
+c++;
+}
+while (d <= 'Z')
+{
+putchar(d);
+d++;
+}
+putchar('\n');
+return (0);
 }
