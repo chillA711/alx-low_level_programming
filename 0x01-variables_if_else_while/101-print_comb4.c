bb0936da70f2981dec07d677feb2c9769edca2fa
chillA711 <emj7job@gmail.com>
2022-12-13 13:43:15 -0800

    the success combination in business is do what you do better and do more of what you do

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,20 +15,16 @@ for (tens = '0'; tens <= '9'; tens++)
 {
 for (ones = '0'; ones <= '9'; ones++)
 {
-if (!((ones == tens) || (tens == hundreds) || (tens > ones) || (hundreds > tens)))
-{
+if (!((ones == tens) || (tens == hundreds) || (tens > ones) || (hundreds > tens))){
 putchar(hundreds);
 putchar(tens);
 putchar(ones);
-if (!(ones == '9' && hundreds == '7' && tens == '8'))
-{
+if (!(ones == '9' && hundreds == '7' && tens == '8')){
 putchar(',');
-putchar(' ');
-}
+putchar(' ');}
 }
 }
 }
 }
 putchar('\n');
-return (0);
-}
+return (0);}
